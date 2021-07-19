11f9c698259f768c6ad2051a544d2cdec6e1e07c
Oisín_windows <byrne.ois@gmail.com>
2021-07-19 12:36:38 +0100

    fallback page

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import {createClient} from 'contentful'
 import Image from 'next/image'
 import {documentToReactComponents} from '@contentful/rich-text-react-renderer'
+import Skeleton from '../../components/Skeleton'
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -40,6 +41,10 @@ export const getStaticProps = async ({params}) =>{
 
 
 export default function RecipeDetails({recipe}) {
+
+  if(!recipe){
+    return ( <Skeleton/>)
+  }
 
   const {featuredImage, title, cookingTime, ingredients, method } = recipe.fields
 
